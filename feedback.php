f020a65c5a4d937de80e9d1e1cd8162e6482c7cc
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-07-31 11:13:59 +0100

    Updated feedback
    Added hard-coded database
    Created Database

--- feedback.php
+++ feedback.php
@@ -1,22 +1,41 @@
 <?php include "inc/header.php"; ?>
+
+<?php 
+$feedback = [
+  [
+    "id" => "1",
+    "name" => "Beth Williams",
+    "email" => "beth@gmail.com",
+    "body" => "Coding is for LIFE"
+  ],
+  [
+    "id" => "2",
+    "name" => "Lucy Luck",
+    "email" => "lucy@gmail.com",
+    "body" => "Coding can change your life"
+  ],
+  [
+    "id" => "3",
+    "name" => "Ross Johnsom",
+    "email" => "ross@gmail.com",
+    "body" => "Coding is for me"
+  ]
+  ];
+
+  ?>
    
-    <h2>Feedback</h2>
+    <h2>Past Feedback</h2>
+    <?php if(empty($feedback)) : ?>
+      <p class="lead mt3">There is no feedback</p>
+    <?php endif; ?>
 
-    <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+    <?php foreach($feedback as $item): ?>
+
+    <div class="card my-3 w-75">
+     <div class="card-body text-center">
+      <?php echo $item["body"]; ?>
      </div>
    </div>
 
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
-
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
+   <?php endforeach; ?>
    <?php include "inc/footer.php"; ?>
